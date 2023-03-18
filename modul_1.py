c4c30db98fe78c0390a66eac6c3a8267ebe9d730
Bima Randy <121949691+Bima911@users.noreply.github.com>
2023-03-18 21:07:50 +0700

    Add files via upload

---
+++ modul_1.py
@@ -0,0 +1,10 @@
+def greeting(name):
+    print("Hello, " + name) 
+
+person1 = {
+    "name": "Bima Randy Gumilang",
+    "NIM": 312010497,
+    "class": "TI20E1",
+    "age": 23,
+    "country": "Indonesia"
+}
